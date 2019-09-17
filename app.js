3e988e97376102c500a3e21d3ec8c016836ae41b
enniogit <ennio_picarelli@it.ibm.com>
2019-09-17 17:12:53 +0200

    Update app.js
    xxxx

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-
-	// print a message when the server starts listening
+app.listen(8080, '0.0.0.0', function() {
+// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
